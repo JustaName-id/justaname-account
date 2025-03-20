511511a737aad915faf8bba3cd87e12d2f5cc696
AngeloAyranji <angeloayranji@gmail.com>
2025-03-20 14:43:58 +0200

    test: add general and 7702 unit test for justaname account contract

---
+++ test/unit/Test7702JustaNameAccount.t.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {Test, console, Vm} from "forge-std/Test.sol";
+import {IAccount} from "@account-abstraction/interfaces/IAccount.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {BaseAccount} from "@account-abstraction/core/BaseAccount.sol";
+
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {CodeConstants} from "../../script/HelperConfig.s.sol";
+import {DeployJustaNameAccount} from "../../script/DeployJustaNameAccount.s.sol";
+import {JustaNameAccount} from "../../src/JustaNameAccount.sol";
+
+contract TestJustaNameAccount is Test, CodeConstants {
+    JustaNameAccount public justaNameAccount;
+    HelperConfig public helperConfig;
+    ERC20Mock public mockERC20;
+
+    address public entryPointAddress;
+
+    function setUp() public {
+        DeployJustaNameAccount deployer = new DeployJustaNameAccount();
+        (justaNameAccount, entryPointAddress) = deployer.run();
+
+        mockERC20 = new ERC20Mock();
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                            EXECUTE TESTS
+    //////////////////////////////////////////////////////////////*/
+    function test_ThrowErrorIfCallingExecuteFromNotEntrypointOrOwner(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) public {
+        vm.expectRevert(abi.encodeWithSelector(JustaNameAccount.JustaNameAccount_NotOwnerorEntryPoint.selector));
+        justaNameAccount.execute(target, value, data);
+    }
+
+    function test_ShouldExecuteCorrectly(
+        address to,
+        uint256 amount
+    ) public {
+        vm.assume(to != address(0));
+
+        bytes memory data = abi.encodeCall(ERC20Mock.mint, (to, amount));
+        
+        vm.signAndAttachDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
+        vm.prank(TEST_ACCOUNT_ADDRESS);
+        JustaNameAccount(TEST_ACCOUNT_ADDRESS).execute(address(mockERC20), 0, data);
+
+        assertEq(mockERC20.balanceOf(to), amount);
+    }
+
+    // function test_ShouldExecuteCorrectlyWithSponsor(
+    //     uint256 amount
+    // ) public {
+    //     (address BOB_ADDRESS, uint256 BOB_PK) = makeAddrAndKey("bob");
+
+    //     bytes memory data = abi.encodeCall(ERC20Mock.mint, (BOB_ADDRESS, amount));
+
+    //     Vm.SignedDelegation memory signedDelegation = vm.signDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
+    
+    //     vm.broadcast(BOB_PK);
+    //     vm.attachDelegation(signedDelegation);
+    //     JustaNameAccount(TEST_ACCOUNT_ADDRESS).execute(address(mockERC20), 0, data);
+
+    //     assertEq(mockERC20.balanceOf(BOB_ADDRESS), amount);
+    // }
+
+
+    /*//////////////////////////////////////////////////////////////
+                          EXECUTE BATCH TESTS
+    //////////////////////////////////////////////////////////////*/
+    function test_ThrowErrorIfCallingExecuteBatchFromNotEntrypointOrOwner(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) public {
+        vm.assume(target != address(0));
+
+        BaseAccount.Call[] memory calls = new BaseAccount.Call[](1);
+        calls[0] = BaseAccount.Call({
+            target: target,
+            value: value,
+            data: data
+        });
+
+        vm.expectRevert(abi.encodeWithSelector(JustaNameAccount.JustaNameAccount_NotOwnerorEntryPoint.selector));
+        justaNameAccount.executeBatch(calls);
+    }
+
+    function test_ShouldExecuteBatchCorrectly(
+        address to,
+        uint256 amount
+    ) public {
+        vm.assume(to != address(0));
+
+        bytes memory data1 = abi.encodeCall(ERC20Mock.mint, (to, amount));
+        bytes memory data2 = abi.encodeCall(ERC20Mock.burn, (to, amount));
+
+        BaseAccount.Call[] memory calls = new BaseAccount.Call[](2);
+        calls[0] = BaseAccount.Call({
+            target: address(mockERC20),
+            value: 0,
+            data: data1
+        });
+        calls[1] = BaseAccount.Call({
+            target: address(mockERC20),
+            value: 0,
+            data: data2
+        });
+
+        vm.signAndAttachDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
+        vm.prank(TEST_ACCOUNT_ADDRESS);
+        JustaNameAccount(TEST_ACCOUNT_ADDRESS).executeBatch(calls);
+
+        assertEq(mockERC20.balanceOf(to), 0);
+    }
+}
--- test/unit/TestGeneralJustaNameAccount.t.sol
+++ test/unit/TestGeneralJustaNameAccount.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, Vm} from "forge-std/Test.sol";
 import {IAccount} from "@account-abstraction/interfaces/IAccount.sol";
 import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
@@ -13,32 +13,7 @@ import {CodeConstants} from "../../script/HelperConfig.s.sol";
 import {DeployJustaNameAccount} from "../../script/DeployJustaNameAccount.s.sol";
 import {JustaNameAccount} from "../../src/JustaNameAccount.sol";
 
-contract ERC20 {
-    address public minter;
-    mapping(address => uint256) private _balances;
-
-    constructor(address _minter) {
-        minter = _minter;
-    }
-
-    function mint(uint256 amount, address to) public {
-        _mint(to, amount);
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function _mint(address account, uint256 amount) internal {
-        require(msg.sender == minter, "ERC20: msg.sender is not minter");
-        require(account != address(0), "ERC20: mint to the zero address");
-        unchecked {
-            _balances[account] += amount;
-        }
-    }
-}
-
-contract TestJustaNameAccount is Test, CodeConstants {
+contract TestGeneralJustaNameAccount is Test, CodeConstants {
     JustaNameAccount public justaNameAccount;
     HelperConfig public helperConfig;
 
@@ -49,12 +24,15 @@ contract TestJustaNameAccount is Test, CodeConstants {
         (justaNameAccount, entryPointAddress) = deployer.run();
     }
 
-    function test_ShouldReturnCorrectEntryPoint() public {
+    /*//////////////////////////////////////////////////////////////
+                          ENTRYPOINT TESTS
+    //////////////////////////////////////////////////////////////*/
+    function test_ShouldReturnCorrectEntryPoint() public view {
         address _entryPoint = address(justaNameAccount.entryPoint());
         assertEq(_entryPoint, entryPointAddress);
     }
 
-    function test_ShouldReturnTrueIfCorrectInterface() public {
+    function test_ShouldReturnTrueIfCorrectInterface() public view {
         assertTrue(
             justaNameAccount.supportsInterface(type(IAccount).interfaceId)
         );
@@ -76,9 +54,12 @@ contract TestJustaNameAccount is Test, CodeConstants {
         );
     }
 
+    /*//////////////////////////////////////////////////////////////
+                        SUPPORTS INTERFACE TESTS
+    //////////////////////////////////////////////////////////////*/
     function test_ShouldReturnFalseIfIncorrectInterface(
         bytes4 _interfaceId
-    ) public {
+    ) public view {
         vm.assume(_interfaceId != type(IAccount).interfaceId);
         vm.assume(_interfaceId != type(IERC165).interfaceId);
         vm.assume(_interfaceId != type(IERC1271).interfaceId);
@@ -88,14 +69,20 @@ contract TestJustaNameAccount is Test, CodeConstants {
         assertFalse(justaNameAccount.supportsInterface(_interfaceId));
     }
 
+    /*//////////////////////////////////////////////////////////////
+                          VALID SIGNATURE TESTS
+    //////////////////////////////////////////////////////////////*/
     function test_ShouldReturnFalseIfIncorrectSignature(
         bytes32 messageHash
     ) public {
-        (address alice, uint256 alicePk) = makeAddrAndKey("alice");
+        (, uint256 alicePk) = makeAddrAndKey("alice");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, messageHash);
         bytes memory badSignature = abi.encodePacked(r, s, v);
-        
-        vm.signAndAttachDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
+
+        vm.signAndAttachDelegation(
+            address(justaNameAccount),
+            TEST_ACCOUNT_PRIVATE_KEY
+        );
         bytes4 result = JustaNameAccount(TEST_ACCOUNT_ADDRESS).isValidSignature(
             messageHash,
             badSignature
@@ -106,33 +93,20 @@ contract TestJustaNameAccount is Test, CodeConstants {
     function test_ShouldReturnTrueIfSignatureIsValid(
         bytes32 messageHash
     ) public {
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(TEST_ACCOUNT_PRIVATE_KEY, messageHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            TEST_ACCOUNT_PRIVATE_KEY,
+            messageHash
+        );
         bytes memory signature = abi.encodePacked(r, s, v);
 
-        vm.signAndAttachDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
+        vm.signAndAttachDelegation(
+            address(justaNameAccount),
+            TEST_ACCOUNT_PRIVATE_KEY
+        );
         bytes4 result = JustaNameAccount(TEST_ACCOUNT_ADDRESS).isValidSignature(
             messageHash,
             signature
         );
         assertEq(result, bytes4(0x1626ba7e));
-    }
-
-    function test_ThrowErrorIfCallingExecuteFromNonEntrypointOrOwner(
-        address target,
-        uint256 value,
-        bytes calldata data
-    ) public {
-        vm.expectRevert(abi.encodeWithSelector(JustaNameAccount.JustaNameAccount_NotOwnerorEntryPoint.selector));
-        justaNameAccount.execute(target, value, data);
-    }
-
-    function test_ShouldExecuteCorrectly(
-        address to
-    ) public {
-        ERC20 erc20 = new ERC20(address(TEST_ACCOUNT_ADDRESS));
-        bytes memory data = abi.encodeCall(ERC20.mint, (100, to));
-        
-        vm.signAndAttachDelegation(address(justaNameAccount), TEST_ACCOUNT_PRIVATE_KEY);
-        JustaNameAccount(TEST_ACCOUNT_ADDRESS).execute(address(erc20), 0, data);
     }
 }
